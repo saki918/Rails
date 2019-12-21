41e8f0be512e02f0fd44939f59031638098a5cc3
saki918 <hiro9181013@gmail.com>
2019-12-21 05:36:41 +0000

    [finish]3-4

--- db/migrate/20191221043324_create_lists.rb
+++ db/migrate/20191221043324_create_lists.rb
@@ -1,7 +1,9 @@
 class CreateLists < ActiveRecord::Migration[5.2]
   def change
     create_table :lists do |t|
-
+     # 以下の2行を追加
+      t.string :title
+      t.string :body
       t.timestamps
     end
   end
