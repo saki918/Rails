ce8007b1f210ea51b2476acf6657413a7860f2c6
saki918 <hiro9181013@gmail.com>
2019-12-21 09:56:44 +0000

    [finish]3-7

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -17,19 +17,28 @@ class TodolistsController < ApplicationController
   end
 
   def index
-  	  @lists = List.all
+  	   @lists = List.all
   end
 
   def show
 
-  	@list = List.find(params[:id])
+  	   @list = List.find(params[:id])
   	
   end
+  def edit
+       @list = List.find(params[:id])
+  end
+
+  def update
+  	list = List.find(params[:id])
+        list.update(list_params)
+        redirect_to todolist_path(list.id)
+    end
 
   private
    
   def list_params
-      params.require(:list).permit(:title, :body)
+       params.require(:list).permit(:title, :body)
   end
 
 end
