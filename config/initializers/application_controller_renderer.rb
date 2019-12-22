91870948d9b7ccc067654a49f5af4ca44abd6310
saki918 <hiro9181013@gmail.com>
2019-12-22 03:02:56 +0000

    [finish]3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'cae3d8e19c057a16b3d8cd66246873361cbe25e256e0d8ee0b495d436933c99bd52c50d5b9ae72d2a206086170568d965c6cf23b5c71b12ed400305525b5b0e2'
